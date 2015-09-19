4cdec62093ca5de455af02a7ae5126881a8d34b2
Jimmy <liangshao_ls@126.com>
2015-09-19 09:51:23 +0800

    chnage index.php file

--- index.php
+++ index.php
@@ -17,10 +17,16 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
-//绑定Admin模块到当前入口文件 运行时会自动创建该模块
-define('BIND_MODULE','Admin');
 // 定义应用目录
-define('APP_PATH','./Application/');
+//define('APP_PATH','./Application/');
+define('APP_PATH','./MyApp/');
+//运行时目录
+define('RUNTIME_PATH','./MyApp/Runtime/');
+
+//每个目录生成的的安全文件
+//define('DIR_SECURE_FILENAME', 'default.html');
+//不生成安全文件
+//define('BUILD_DIR_SECURE', false);
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
